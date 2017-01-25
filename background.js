8a5e8ba41f4e2195e9214dc7d5f7b74d83b716fb
Wagner Moschini <wagnermoschini@gmail.com>
2017-01-25 18:57:29 -0200

    add first version

---
+++ background.js
@@ -0,0 +1,31 @@
+var grayscale = false;
+
+function dorialize() {
+  if(grayscale){
+    chrome.tabs.executeScript({
+      code: 'document.body.style.filter = ""'
+    });
+    chrome.browserAction.setIcon({
+      path:"icon_colored.jpg"
+    });
+  } else {
+    chrome.tabs.executeScript({
+      code: 'document.body.style.filter = "grayscale()"'
+    });
+    chrome.browserAction.setIcon({
+      path:"icon_grayscale.jpg"
+    });
+  }
+  grayscale = !grayscale;
+  
+}
+
+
+chrome.browserAction.onClicked.addListener(dorialize);
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   chrome.tabs.executeScript({
+//     code: 'document.body.style.filter = "grayscale()"'
+//   });
+// });
+
+
