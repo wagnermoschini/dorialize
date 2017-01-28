7c8f3f46f4d7d8460db62b8fd7ec4b12710d0d3e
Wagner Moschini <wagnermoschini@gmail.com>
2017-01-28 16:02:21 -0200

    implement gray images funcionality

--- background.js
+++ background.js
@@ -1,16 +1,19 @@
 var grayscale = false;
 
+var resetCSS = 'document.body.style.filter = "";for(var i=0;i<document.images.length;i++){document.images[i].style.setProperty("filter","")}';
+var dorializeCSS = 'document.body.style.filter = "grayscale()";for(var i=0;i<document.images.length;i++){document.images[i].style.setProperty("filter","contrast(0)")}';
+
 function dorialize() {
   if(grayscale){
     chrome.tabs.executeScript({
-      code: 'document.body.style.filter = ""'
+      code: resetCSS
     });
     chrome.browserAction.setIcon({
       path:"icon_colored.jpg"
     });
   } else {
     chrome.tabs.executeScript({
-      code: 'document.body.style.filter = "grayscale()"'
+      code: dorializeCSS
     });
     chrome.browserAction.setIcon({
       path:"icon_grayscale.jpg"
